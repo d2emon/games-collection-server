d3c89b57020aed0d14dee21165027c69cad7a7a5
Dmitry Kutsenko <d2emonium@gmail.com>
2017-12-30 23:20:10 +0300

    REST CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,35 +3,72 @@ var router = express.Router()
 
 var models = require('../models')
 
+function showError(res, err) {
+  res.send({
+    error: err
+  })
+  return console.error(err)
+}
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' })
 })
 
+router.get('/games', (req, res) => {
+  models.Game.find({}, (err, models) => {
+    if (err) return console.error(err)
+
+    res.send(models)
+  })
+})
+
 router.post('/games', (req, res) => {
-  console.log(req.body)
   var note = new models.Game({
     title: req.body.title,
     body: req.body.body
   })
 
   note.save((err, model) => {
-    if (err) {
-      res.send({
-        error: err
-      })
-      return console.error(err)
-    }
+    if (err) return showError(res, err)
 
     res.send(model)
   })
+})
 
-  models.Game.find({}, (err, models) => {
-    if (err) return console.error(err)
+router.get('/games/:id', (req, res) => {
+  const id = req.params.id
 
-    console.log('Found')
-    console.log(models)
+  models.Game.findById(id, (err, model) => {
+    if (err) return showError(res, err)
+
+    res.send(model)
   })
 })
 
+router.delete('/games/:id', (req, res) => {
+  const id = req.params.id
+
+  models.Game.findByIdAndRemove(id, (err, model) => {
+    if (err) return showError(res, err)
+
+    res.send({ 'success': true })
+  })
+})
+
+router.put('/games/:id', (req, res) => {
+  const id = req.params.id
+
+  models.Game.findById(id, (err, model) => {
+    if (err) return showError(res, err)
+
+    model.title = req.body.title
+    model.body = req.body.body
+    model.save((err, model) => {
+      if (err) return showError(res, err)
+
+      res.send(model)
+    })
+  })
+})
 module.exports = router
